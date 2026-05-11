58aab55fd9e803d236dce3b669003f608cf4d92f
Abinash Pradhan <abinashpradhansu@gmail.com>
2026-05-11 05:44:43 +0000

    dataTypes

---
+++ javaScript/dataTypes.js
@@ -0,0 +1,29 @@
+ // primitive dataTypes call by value
+
+ /* there are 7 types of Primitive dataTypes String ,
+ Number,Null,undefined,Symbol,BigInt */
+
+//  nonPrimitive
+
+// call by refrence  mainly 3 types objects,array,functions
+
+//Symbol 
+
+
+// const id = Symbol('123');
+// const anotherId =Symbol('123')
+// // both are diffeent due to use of Symbol
+
+// console.log(typeof(id));   // Symbol
+// console.log(anotherId);  // Symbol
+
+
+let BigNumber = 2445467146444449889932046n;
+ let number= null;
+//  console.log(number);  //  null
+//  console.log(undefined);   // undefined
+ let num;
+//  console.log(num);  // undfined
+ 
+
+  
